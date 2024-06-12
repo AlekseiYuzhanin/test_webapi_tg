8f247a213b804b7d9814eedd0653721de362bdd2
alexey <aleksei.yuiuzhanin@yandex.ru>
2024-06-12 21:37:34 +0700

    m

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useTelegram} from "./hooks/useTelegram";
 import {useEffect} from "react";
+import Header from "./components/Header/Header";
 
 function App() {
     const {tg,onToggleButton} = useTelegram();
@@ -10,6 +11,7 @@ function App() {
     },[tg]);
   return (
     <div className="App">
+        <Header></Header>
         <button onClick={onToggleButton}>toggle</button>
     </div>
   );
