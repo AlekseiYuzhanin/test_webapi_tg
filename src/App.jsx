d20f96b696106db3b9e7f0e091da4851a4e42d5e
alexey <aleksei.yuiuzhanin@yandex.ru>
2024-06-12 22:47:26 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css';
-import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-import {Route, Router, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import ProductsList from "./components/ProductsList/ProductsList";
 import Form from "./components/Form/Form";
 
 
 function App() {
-    const {onToggleButton} = useTelegram();
-
 
   return (
     <div className="App">
