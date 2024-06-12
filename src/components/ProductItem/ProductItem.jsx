897cd36b6c0c736821eef255221a4b755a3e259f
alexey <aleksei.yuiuzhanin@yandex.ru>
2024-06-13 00:16:00 +0700

    conn

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -15,7 +15,7 @@ const ProductItem = ({product, className, onAdd}) => {
             <div className={'price'}>
                 <span>Стоимость: <b>{product.price}</b></span>
             </div>
-            <Button onClick={onAddHandler}>
+            <Button className={'add-btn'} onClick={onAddHandler}>
                 Добавить в корзину
             </Button>
         </div>
