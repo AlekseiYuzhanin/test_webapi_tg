6709662079f85bb6a77b1408a582eab5841f28d1
alexey <aleksei.yuiuzhanin@yandex.ru>
2024-06-12 22:56:14 +0700

    conn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     const { user, onClose } = useTelegram();
 
     return (
-        <div className={"header"} >
+        <div className={"header"}>
             <Button onClick={onClose}>Close</Button>
             <span className={'username'}>{user?.username}</span>
         </div>
