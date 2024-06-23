44ef6e6027bb41af2fd54b263caf70da7375fc76
alexey <aleksei.yuiuzhanin@yandex.ru>
2024-06-23 21:30:24 +0700

    commit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,10 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import './Form.css'
 import {useTelegram} from "../../hooks/useTelegram";
-import Button from "../Button/Button";
 
 const Form = () => {
     const [country, setCountry] = useState('');
     const [street, setStreet] = useState('');
-
     const {tg} = useTelegram();
 
     const onSendData = useCallback(() => {
@@ -15,58 +13,53 @@ const Form = () => {
             street,
         }
         tg.sendData(JSON.stringify(data));
-    },[country,street,tg])
+    }, [country, street,tg])
 
     useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData);
+        tg.onEvent('mainButtonClicked', onSendData)
         return () => {
-            tg.offEvent('mainButtonClicked', onSendData);
+            tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [tg, onSendData]);
+    }, [onSendData,tg])
 
     useEffect(() => {
         tg.MainButton.setParams({
-            text: 'Отправить данные',
-
+            text: 'Отправить данные'
         })
-    }, [tg.MainButton]);
+    }, [tg.MainButton])
 
     useEffect(() => {
-        if(!street || !country){
+        if(!street || !country) {
             tg.MainButton.hide();
-        }else{
+        } else {
             tg.MainButton.show();
         }
-    }, [country,street,tg.MainButton]);
-
+    }, [country, street,tg.MainButton])
 
     const onChangeCountry = (e) => {
-        setCountry(e.target.value);
+        setCountry(e.target.value)
     }
 
     const onChangeStreet = (e) => {
-        setStreet(e.target.value);
+        setStreet(e.target.value)
     }
 
+
     return (
-        <div className={"form"} >
-            <h3>Откуда узнал о команде?</h3>
-            <br></br>
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
             <input
                 className={'input'}
                 type="text"
                 value={country}
-                onChange={onChangeCountry}/>
-            <br></br>
-            <h3>Есть ли опыт работы в данной тематике?</h3>
-            <br></br>
+                onChange={onChangeCountry}
+            />
             <input
                 className={'input'}
                 type="text"
                 value={street}
-                onChange={onChangeStreet}/>
-            <br></br>
-            <Button onClick={onSendData}>knopka</Button>
+                onChange={onChangeStreet}
+            />
         </div>
     );
 };
